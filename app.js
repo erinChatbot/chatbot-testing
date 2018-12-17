32e2d627f70b441063c8a4fbeaa3d70847de6e65
Erin <erinfan@motherapp.com>
2018-12-17 15:49:59 +0800

    [WIP] define postback payload

--- app.js
+++ app.js
@@ -144,9 +144,9 @@ function handlePostback (sender_psid, received_postback) {
   //   response = { 'text': 'Oops, try sending another image.' }
   // }
 
-  if (payload == 'USER_DEFINED_PAYLOAD') {
-  response = { 'text': '你好呀親，仲未做好~' }
-}
+  if (payload === 'USER_DEFINED_PAYLOAD') {
+    response = { 'text': '你好呀親，仲未做好~' }
+  }
 
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response)
