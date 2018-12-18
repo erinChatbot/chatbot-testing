74aaaa1b74293524cdc61f1bda074d5081c70edb
Erin <erinfan@motherapp.com>
2018-12-18 17:41:45 +0800

    [WIP] code refactor

--- sendResponse.js
+++ sendResponse.js
@@ -3,36 +3,36 @@ var logger = require('./log');
 
 var sendResponse = {
     sendGetStartedMsg: function(recipientId) {
-        logger.info('sendResponse | sendGetStartedMsg')
-//        var msg1 = 'Hi，我係Loyalty Chatbot\uD83D\uDC4B';
-//        var msg2 = '我試緊堆APIsss';
-//        app.sendTextMessageWithoutQuickReply(recipientId, msg1);
-//        setTimeout(function() {
-//            app.sendTextMessageWithoutQuickReply(recipientId, msg2);
-//         }, 1000)
-//        var messageData = {
-//            recipient: {
-//                id: recipientId
-//            }
-//            message: {
-//                text: "第一次用？需唔需要教學示範？",
-//                quick_replies: [
-//                    {
-//                        "content_type":"text",
-//                        "title":"好啊",
-//                        "payload":"PAYLOAD_FOR_NEED_TUTORIAL"
-//                    },
-//                    {
-//                        "content_type":"text",
-//                        "title":"唔洗啦",
-//                        "payload":"PAYLOAD_FOR_NO_TUTORIAL"
-//                    }
-//                ]
-//            }
-//        };
-//        setTimeout(function() {
-//            app.callSendAPI(messageData);
-//        }, 2000)
+        logger.info('sendResponse | sendGetStartedMsg');
+        var msg1 = 'Hi，我係Loyalty Chatbot\uD83D\uDC4B';
+        var msg2 = '我試緊堆APIsss';
+        app.sendTextMessageWithoutQuickReply(recipientId, msg1);
+        setTimeout(function() {
+            app.sendTextMessageWithoutQuickReply(recipientId, msg2);
+        }, 1000)
+        var messageData = {
+            recipient: {
+                id: recipientId
+            },
+            message: {
+                text: "第一次用？需唔需要教學示範？",
+                quick_replies: [
+                    {
+                        "content_type":"text",
+                        "title":"好啊",
+                        "payload":"PAYLOAD_FOR_NEED_TUTORIAL"
+                    },
+                    {
+                        "content_type":"text",
+                        "title":"唔洗啦",
+                        "payload":"PAYLOAD_FOR_NO_TUTORIAL"
+                    }
+                ]
+            }
+        };
+        setTimeout(function() {
+            app.callSendAPI(messageData);
+        }, 2000)
     }
 };
 
