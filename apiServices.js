f35a861756256e4842b63948a2c8989036832dc2
Erin <erinfan@motherapp.com>
2018-12-27 16:20:45 +0800

    [WIP] code refactor

--- apiServices.js
+++ apiServices.js
@@ -1,9 +1,8 @@
 const request = require(‘request’);
-var apiHelper = require('./apiHelper');
 
 module.exports = {
     getFeaturedCampaign: function() {
-        request(apiHelper.GET_FEATURED_CAMPAIGN, { json: true }, (err, res, body) => {
+        request('http://connector.uat.aillia.motherapp.com/api/customer/campaign/featured', { json: true }, (err, res, body) => {
         if (err) { return console.log(err); }
         console.log(body.url);
         console.log(body.explanation);
