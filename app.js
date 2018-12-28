40d43a4078a923319647a46507615b4e9950c311
Erin <erinfan@motherapp.com>
2018-12-28 15:55:44 +0800

    [WIP] code refactor

--- app.js
+++ app.js
@@ -637,11 +637,16 @@ function campaignOffer(recipientId) {
     logger.info('custom Function campaignOffer');
     apiService.getFeaturedCampaign(function(apiResult){
         for(var i=0; i < apiResult.length; i++) {
-            var imageUrl = "";
+            var campaignTitle = apiResult[i].name;
+            var campaignDesc = "無";
+            var imageUrl = "無";
             if (typeof apiResult[i].photos[0] !== 'undefined' && apiResult[i].photos[0] !== null) {
                 imageUrl = util.format('https://connector.uat.aillia.motherapp.com/api/campaign/%s/photo/%s', apiResult[i].campaignId, apiResult[i].photos[0].photoId.id);
             }
-            sendTextMessage(recipientId, "第"+i+"個campaign title係："+apiResult[i].name+"\nshort description: "+apiResult[i].shortDescription+"\nimage url: "+imageUrl);
+            if (typeof apiResult[i].shortDescription !== 'undefined' && apiResult[i].shortDescription !== null) {
+                campaignDesc = apiResult[i].shortDescription
+            }
+            sendTextMessage(recipientId, "第"+i+"個campaign title係："+campaignTitle+"\n\nshort description: "+campaignDesc+"\n\nimage url: "+imageUrl);
         }
 //https://connector.uat.aillia.motherapp.com/api/campaign/{campaign_id}/photo/{photo_id}
 //https://connector.uat.aillia.motherapp.com/api/campaign/e7575bf9-15ab-4d03-9b2d-e0a0a853a2a2/photo/41eaee67-bb8d-41c3-9019-9693388e98c7
