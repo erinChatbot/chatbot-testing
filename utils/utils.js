fb681bf60feade5411627217c5e2fbb48d76d32e
Erin <erinfan@motherapp.com>
2019-01-11 10:48:57 +0800

    [WIP] added utils function

---
+++ utils/utils.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const config = require('config');
+var logger = require('../log');
+
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+  config.get('pageAccessToken');
+
+var facebookApi = "https://graph.facebook.com/v2.6/";
+
+module.exports = {
+    getUserLocale: function(userId, callback) {
+        request.get(facebookApi+userId+"?fields=locale&access_token="+PAGE_ACCESS_TOKEN, (error, response, body) => {
+            if(error) {
+                logger.error(err);
+                return console.log(error);
+            }
+            console.log(JSON.parse(body));
+            logger.info('response body: '+JSON.stringify(body));
+
+//            var apiResult = JSON.parse(body).campaigns
+//            callback(apiResult)
+        });
+    }
+}
