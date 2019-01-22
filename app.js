de0ada31c770f9d817bd1f91886dbf6d5ba50aac
Erin <erinfan@motherapp.com>
2019-01-22 13:58:31 +0800

    [WIP] updated campaign photo path for prod

--- app.js
+++ app.js
@@ -982,7 +982,8 @@ function showCampaign(recipientId, categoryId) {
                     var campaignDesc = "無";
                     var imageUrl = "https://www.sylff.org/wp-content/uploads/2016/04/noImage.jpg";
                     if (typeof apiResult[i].photos[0] !== 'undefined' && apiResult[i].photos[0] !== null) {
-                        imageUrl = util.format('https://backend.sit.aillia.motherapp.com/api/campaign/%s/photo/%s', apiResult[i].campaignId, apiResult[i].photos[0].photoId.id);
+//                        imageUrl = util.format('https://backend.sit.aillia.motherapp.com/api/campaign/%s/photo/%s', apiResult[i].campaignId, apiResult[i].photos[0].photoId.id); //SIT
+                        imageUrl = 'https://middleware.prod.loyalty.motherapp.com/api/campaign/'+apiResult[i].campaignId+'/photo/0'; //PROD
                     }
                     if (typeof apiResult[i].shortDescription !== 'undefined' && apiResult[i].shortDescription !== null) {
                         campaignDesc = apiResult[i].shortDescription
@@ -1042,7 +1043,8 @@ function showCampaign(recipientId, categoryId) {
                     var campaignDesc = "無";
                     var imageUrl = "https://www.sylff.org/wp-content/uploads/2016/04/noImage.jpg";
                     if (typeof apiResult[i].photos[0] !== 'undefined' && apiResult[i].photos[0] !== null) {
-                        imageUrl = util.format('https://backend.sit.aillia.motherapp.com/api/campaign/%s/photo/%s', apiResult[i].campaignId, apiResult[i].photos[0].photoId.id);
+//                        imageUrl = util.format('https://backend.sit.aillia.motherapp.com/api/campaign/%s/photo/%s', apiResult[i].campaignId, apiResult[i].photos[0].photoId.id); //SIT
+                        imageUrl = 'https://middleware.prod.loyalty.motherapp.com/api/campaign/'+apiResult[i].campaignId+'/photo/0'; //PROD
                     }
                     if (typeof apiResult[i].shortDescription !== 'undefined' && apiResult[i].shortDescription !== null) {
                         campaignDesc = apiResult[i].shortDescription
