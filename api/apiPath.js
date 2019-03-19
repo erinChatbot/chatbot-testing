7d974ec6f4de3b29935396814ebbf3b12006d227
Erin <erinfan@motherapp.com>
2019-03-19 14:36:42 +0800

    [chore] updated api structure

--- api/apiPath.js
+++ api/apiPath.js
@@ -4,9 +4,9 @@ let path = {
     // Authenticate
     AUTHENTICATE : '/api/customer/login',
     // Get Customer Status
-    GET_CUSTOMER_STATUS : '/api/customer/status?_locale=+userLocale',
+    GET_CUSTOMER_STATUS : '/api/customer/status?_locale=',
     // Get Campaign Category
-    GET_CAMPAIGN_CATEGORY : '/api/campaignCategory',
+    GET_CAMPAIGN_CATEGORY : '/api/campaign_category',
     // Get Campaign By Category
     GET_CAMPAIGN_BY_CATEGORY : '/api/customer/campaign/available?perPage=5&page=1&categoryId[]=',
     // Get Featured Campaign
