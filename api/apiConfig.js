095f7deef6a397019edd315a01a11856c0119d59
Erin <erinfan@motherapp.com>
2019-03-19 15:11:13 +0800

    [refactor] added api config

--- api/apiConfig.js
+++ api/apiConfig.js
@@ -3,6 +3,16 @@
 let apiPath = {
     // Authenticate
     AUTHENTICATE : '/api/customer/login',
+    // Get Customer Status
+    GET_CUSTOMER_STATUS : '/api/customer/status?_locale=',
+    // Get Campaign Category
+    GET_CAMPAIGN_CATEGORY : '/api/campaign_category',
+    // Get Campaign By Category
+    GET_CAMPAIGN_BY_CATEGORY : '/api/customer/campaign/available?perPage=5&page=1&categoryId[]=',
+    // Get Featured Campaign
+    GET_FEATURED_CAMPAIGN : '/api/customer/campaign/available?isFeatured=true&perPage=5&page=1',
+    // Get Exclusive Campaign
+    GET_EXCLUSIVE_CAMPAIGN : '/api/customer/campaign/available?hasSegment=true&perPage=5&page=1',
 }
 
 module.exports = Object.freeze(apiPath);
